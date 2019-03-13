89ddd3433d1e67cb282a5e44b2895ac40b945c61
viper1452 <bitemepls23@gmail.com>
2019-03-13 01:58:10 +0000

    First Commit

---
+++ Epons/app/src/main/java/com/epons/epons/AmountActivity.java
@@ -0,0 +1,13 @@
+package com.epons.epons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AmountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amount);
+    }
+}
---
+++ Epons/app/src/main/java/com/epons/epons/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.epons.epons;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut = 4000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashTimeOut);
+
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
